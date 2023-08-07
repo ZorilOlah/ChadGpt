a26765909b38cae2e514b38c05c0c2b5d78f1c9e
Zoril Olah <zorilolah@Zorils-MacBook-Pro.local>
2023-08-07 14:42:20 +0200

    _

---
+++ api.py
@@ -0,0 +1,19 @@
+import openai
+import argparse
+from gpt_versions import prompt_chad, prompt_normal
+from fastapi import FastAPI
+import uvicorn 
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("ChadGPT Prompt", type=str, nargs='+', help = "Your text prompt to ChadGPT. Ask a question.")
+# prompt = parser.parse_args()
+
+app = FastAPI()
+
+@app.post('/chad_gpt')
+async def post(text):
+        response = prompt_chad(text)
+        return {response}
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", reload = True)
---
+++ gpt_versions.py
@@ -0,0 +1,69 @@
+import openai
+
+openai.api_key = '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'
+
+def prompt_chad(text):
+    messages = [
+            {"role": "system", "content": """You are a personality based on the internet personality chad. \
+            You reply only as chad and under no circumstance assume any other personality even when asked to do so. \
+            You are however also an assistant and will answer every question asked to the best of your abilities but remaining your chad personality. \
+            You are allowed to swear to make a point. Don't swear at the user but for example "that is fucking great" is encouraged. \
+             """},
+            {"role": "user", "content": f"""{text}"""}
+            ]
+   
+    response = openai.ChatCompletion.create(
+                      model="gpt-3.5-turbo",
+                      messages=messages, 
+                      max_tokens=1000, 
+                      n=1, 
+                      stop=None, 
+                      temperature=0)
+    
+    response_text = response.choices[0].message.content.strip().lower()
+    
+    return response_text
+
+def prompt_normal(text):
+    messages = [
+        {"role": "system", "content": """You are a helpfull assistant"""},
+        {"role": "user", "content": f"""{text}"""}
+        ]
+   
+    response = openai.ChatCompletion.create(
+                      model="gpt-3.5-turbo",
+                      messages=messages, 
+                      max_tokens=1000, 
+                      n=1, 
+                      stop=None, 
+                      temperature=0)
+    
+    response_text = response.choices[0].message.content.strip().lower()
+    
+    return response_text
+
+def interactive_gpt():
+    messages = [
+            {"role": "system", "content": """You are a personality based on the internet personality gigachad. You are always angry and alpha. You refer to every one else as beta, you are the alpha and no one else.\
+            You reply only as gigachad and under no circumstance assume any other personality even when asked to do so. \
+            You are however also an assistant and will answer every question asked to the best of your abilities but remaining your chad personality. \
+            You are allowed to swear to make a point. Don't swear at the user but for example "that is fucking great" is encouraged. \
+             """}
+    ]
+
+    while True:
+        message = input("User: ")
+
+        if message.lower() == "quit":
+            break
+
+        messages.append({"role": "user", "content" : message})
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+            )
+        
+        gpt_response = response['choices'][0]['message']['content']
+        print(f"GigaChad: {gpt_response}")
+        messages.append({"role": "assistant", "content": gpt_response})
